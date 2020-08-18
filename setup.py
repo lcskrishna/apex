cf5d60b171fb1fd710b204beb204408da392208d
lcskrishna <lollachaitanya@gmail.com>
2020-08-18 10:07:48 -0700

    update compiler arguments

--- setup.py
+++ setup.py
@@ -281,15 +281,14 @@ if "--xentropy" in sys.argv:
                                        'apex/contrib/csrc/xentropy/xentropy_kernel.cu'],
                               include_dirs=[os.path.join(this_dir, 'csrc')],
                               extra_compile_args={'cxx': ['-O3'] + version_dependent_macros,
-                                                  'nvcc':[]}))
+                                                  'nvcc':['-O3'] + version_dependent_macros}))
         else:
              ext_modules.append(
                 CUDAExtension(name='xentropy_cuda',
                               sources=['apex/contrib/csrc/xentropy/interface.cpp',
                                        'apex/contrib/csrc/xentropy/hip/xentropy_kernel.hip'],
                               include_dirs=[os.path.join(this_dir, 'csrc/hip')],
-                              extra_compile_args={'cxx': ['-O3'] + version_dependent_macros,
-                                                  'nvcc':[]}))
+                              extra_compile_args=['-O3'] + version_dependent_macros))
    
 
 if "--deprecated_fused_adam" in sys.argv:
